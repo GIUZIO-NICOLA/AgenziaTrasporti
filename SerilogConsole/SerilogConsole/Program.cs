0c34364a992762d9f8b781051c3da49d976ee4a2
GIUZIO-NICOLA <giuzio.nicolavincenzo@gmail.com>
2022-04-26 12:37:05 +0200

    Updates

---
+++ SerilogConsole/SerilogConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace SerilogConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .CreateLogger();
+            Log.Logger.Information("Applicatrion starting up....");
+        }
+
+        static IHost Hostbuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+
+                }).Build();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        }
+
+    }
+}
